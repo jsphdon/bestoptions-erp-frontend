cb61fa2d0601b4823a78528b71b284afe29692b2
jsphdon <jdcagampang14@gmail.com>
2024-06-06 16:28:34 +0400

    added children parameter to summary card

--- src/components/DashboardComponents/SummaryCard.js
+++ src/components/DashboardComponents/SummaryCard.js
@@ -1,10 +1,15 @@
-const SummaryCard = ({ className, amount, description, percentage, currency }) => {
+const SummaryCard = ({ className, amount, description, percentage, currency, children }) => {
   return (
-    <div className={`rounded-lg shadow-sm flex flex-col py-4 px-7 border-slate-100 border min-w-[260px] min-h-[240px] ${className}`}>
-      <h1 className="text-3xl font-semibold flex flex-row items-center gap-1">
-        {currency}{amount}<span className="rounded-md bg-green-200 text-green-700 py-1 px-2 text-sm font-semibold">{percentage}%</span>
-      </h1>
-      <p className="text-sm text-gray-400 font-semibold">{description}</p>
+    <div className={`rounded-lg shadow-sm flex flex-col justify-between py-4 px-7 border-slate-100 border min-w-[260px] min-h-[240px] ${className}`}>
+      <div>
+        <h1 className="text-3xl font-semibold flex flex-row items-center gap-1">
+          {currency}{amount}<span className="rounded-md bg-green-200 text-green-700 py-1 px-2 text-sm font-semibold">{percentage}%</span>
+        </h1>
+        <p className="text-sm text-gray-400 font-semibold">{description}</p>
+      </div>
+      <div>
+        {children}
+      </div>
     </div>
   )
 }
